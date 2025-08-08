bd1d114f17d29f2c051636beb8e2c186517ac01b
Robin Quint <rob2309@hotmail.de>
2025-08-08 14:54:18 +0200

    Added tool.Blender.Modifier.has_mirrored_type and set_mirrored_type.

--- src/bonsai/bonsai/tool/blender.py
+++ src/bonsai/bonsai/tool/blender.py
@@ -50,6 +50,7 @@ import ifcopenshell.util.element
 import numpy as np
 import numpy.typing as npt
 from ifcopenshell import entity_instance
+import ifcopenshell.api.pset
 from mathutils import Matrix, Vector
 
 import bonsai.bim
@@ -1220,6 +1221,20 @@ class Blender(bonsai.core.tool.Blender):
         @classmethod
         def is_stair(cls, element: entity_instance) -> bool:
             return tool.Pset.get_element_pset(element, "BBIM_Stair")
+        
+        @classmethod
+        def has_mirrored_type(cls, element: entity_instance, inherit: bool = True) -> entity_instance | None:
+            pset = ifcopenshell.util.element.get_pset(element, "BBIM_MirroredType", "Data", should_inherit=inherit)
+            if pset and (parsed := json.loads(pset)) and "mirrored_type" in parsed:
+                return tool.Ifc.get_entity_by_id(int(parsed["mirrored_type"]))
+            return None
+        
+        @classmethod
+        def set_mirrored_type(cls, element: entity_instance, mirrored_type: entity_instance):
+            pset = tool.Pset.get_element_pset(element, "BBIM_MirroredType")
+            if not pset:
+                pset = ifcopenshell.api.pset.add_pset(tool.Ifc.get(), element, "BBIM_MirroredType")
+            ifcopenshell.api.pset.edit_pset(tool.Ifc.get(), pset, properties={ "Data": json.dumps({ "mirrored_type": mirrored_type.id() }) })
 
         @classmethod
         def is_editing_railing_path(cls, obj: bpy.types.Object):
